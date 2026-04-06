dd6d4355e08a0efd66cccbb4fee3973c2b399ee1
MdMuzahid07 <mdmuzahid7396@gmail.com>
2026-04-06 20:09:28 +0600

    prisma configured, and app.config.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, jwtConfig],
+    }),
+    ThrottlerModule.forRoot([{ ttl: 6000, limit: 100 }]),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
