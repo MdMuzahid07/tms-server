eab1b917ba487986bc030cdb3ac3f4319b80f621
MdMuzahid07 <mdmuzahid7396@gmail.com>
2026-04-07 12:42:10 +0600

    main.ts configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Server running on  http://localhost:${port}/api`);
 }
 bootstrap();
